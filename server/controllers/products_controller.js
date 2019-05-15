5b6116952ac411316f49eed83c77b68e7b99ae40
Ian Kaliszewski <ikalisz@yahoo.com>
2019-05-15 13:54:41 -0600

    HEROKU SUCKS >:(

---
+++ server/controllers/products_controller.js
@@ -0,0 +1,63 @@
+const controller = {
+    create(req, res, next) {
+        const dbInstance = req.app.get('db')
+        const {name, description, price, image_url} = req.body
+
+        dbInstance.create_product([name, description, price, image_url])
+        .then(created => {
+            console.log(`Hit!`)
+            res.status(200).send(created)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500)
+        })
+    },
+    getOne(req, res, next) {
+        const dbInstance = req.app.get('db')
+        dbInstance.read_product(req.params.id)
+        .then(product => {
+            res.status(200).send(product)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500)
+        })
+    },
+    getAll(req, res, next) {
+        const dbInstance = req.app.get('db')
+        console.log(dbInstance.read_products)
+        dbInstance.read_products()
+        .then(products => {
+            res.status(200).send(products)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500)
+        })
+    },
+    update(req, res, next) {
+        const dbInstance = req.app.get('db')
+        dbInstance.update_product([req.params.id, req.query.desc])
+        .then(updated => {
+            res.status(200).send(updated)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500)
+        })
+    },
+    delete(req, res, next) {
+        const dbInstance = req.app.get('db')
+        dbInstance.delete_product(req.params.id)
+        .then(deleted => {
+            res.status(200).send(deleted)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500)
+        })
+    }
+}
+
+module.exports = controller
--- server/index.js
+++ server/index.js
@@ -4,17 +4,23 @@ const massive = require('massive')
 const app = express()
 const {SERVER_PORT} = process.env
 const {CONNECTION_STRING} = process.env
-const controller = require('./controllers/controller')
-
-massive(CONNECTION_STRING)
+const controller = require('./controllers/products_controller')
+console.log(__dirname)
+massive(CONNECTION_STRING, {scripts: __dirname + '/db'})
 .then(dbInstance => {
     app.set('db', dbInstance)
+    console.log('Connected to DB')
 })
 .catch(err => {
     console.log(err)
 })
 
 app.use(express.json())
+app.get('/api/products', controller.getAll)
+app.get('/api/products/:id', controller.getOne)
+app.put('/api/products/:id?desc=...', controller.update)
+app.post('/api/products', controller.create)
+app.delete('/api/products/:id', controller.delete)
 
 app.listen(SERVER_PORT, () => {
     console.log(`Working on port ${SERVER_PORT}`)
